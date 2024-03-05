2f265bae92994fdb183d268ea768a6a3ea7266b8
247786 Skulimowski Szymon <247786@edu.p.lodz.pl>
2024-03-05 12:52:28 +0100

    Added tests

--- FifteenPuzzleGame.py
+++ FifteenPuzzleGame.py
@@ -79,17 +79,16 @@ def bfs_algorithm(start_board, target_board):
     queue.append(start_board)
     testedPositions = set()
     testedPositions.add(tuple(map(tuple, start_board)))
-    for i in range(30):
+    for i in range(15):
         current_board = queue.popleft()
         if checkingBoard(current_board, target_board):
             return current_board
 
-        for move in ["L", "R", "U", "D"]:
+        for move in ["R", "D", "U", "L"]:
             new_board = [row[:] for row in current_board]
             moveFunction(move, new_board)
             if tuple(map(tuple, new_board)) not in testedPositions:
                 queue.append(new_board)
                 testedPositions.add(tuple(map(tuple, new_board)))
-                print(new_board)
 
 bfs_algorithm(dane, positiveDane)
