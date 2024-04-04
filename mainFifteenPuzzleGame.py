878dd713fcd72f3090843b532166b168c16a51b0
szymskul <szymskul@gmail.com>
2024-04-04 03:26:18 +0200

    Add script to solve fifteenPuzzleExamples

--- mainFifteenPuzzleGame.py
+++ mainFifteenPuzzleGame.py
@@ -1,11 +1,11 @@
 import time
-
+import sys
 import ioFunctions
 import globalVariables
 from bfs_algorithm import bfs_algorithm
 from dfs_algorithm import dfs_algorithm
 from aStar import aStar
-
+'''
 def mainFunction(algorithmType, order, fileToRead, fileToWriteSolution, fileToWriteDetailsOfSolution):
     ioFunctions.readFile(fileToRead)
     if(algorithmType == "bfs"):
@@ -25,4 +25,33 @@ def mainFunction(algorithmType, order, fileToRead, fileToWriteSolution, fileToWr
     ioFunctions.writeSolutionFile(fileToWriteSolution)
     ioFunctions.writeDetailsOfSolutionFile(fileToWriteDetailsOfSolution)
 
-mainFunction("bfs", "DRUL", "data.txt" , "solution.txt", "detailsOfSolution.txt")
+mainFunction("bfs", "DRUL", "data.txt" , "solution.txt", "detailsOfSolution.txt")'''
+
+
+def mainFunction(algorithmType, order, fileToRead, fileToWriteSolution, fileToWriteDetailsOfSolution):
+    ioFunctions.readFile(fileToRead)
+
+    start_time = time.time()
+    if algorithmType == "bfs":
+        bfs_algorithm(globalVariables.dane, globalVariables.target_board, order)
+    elif algorithmType == "dfs":
+        globalVariables.order = order
+        dfs_algorithm(globalVariables.dane, "X", 1, globalVariables.target_board)
+    elif algorithmType == "astr":
+        aStar(globalVariables.dane, globalVariables.target_board, order)
+    end_time = time.time()
+
+    globalVariables.spentTime = float((end_time - start_time) * 1000)
+
+    ioFunctions.writeSolutionFile(fileToWriteSolution)
+    ioFunctions.writeDetailsOfSolutionFile(fileToWriteDetailsOfSolution)
+
+
+if __name__ == "__main__":
+    algorithmType = sys.argv[1]
+    order = sys.argv[2]
+    fileToRead = sys.argv[3]
+    fileToWriteSolution = sys.argv[4]
+    fileToWriteDetailsOfSolution = sys.argv[5]
+
+    mainFunction(algorithmType, order, fileToRead, fileToWriteSolution, fileToWriteDetailsOfSolution)
---
+++ solveScript.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+def solveScript(algorithmType, order):
+    directoryToSolve = "toSolve"
+    directoryToSolved = "Solved"
+    directoryToStats = "solvedStats"
+    os.makedirs("toSolve", exist_ok=True)
+    os.makedirs("Solved", exist_ok=True)
+    os.makedirs("solvedStats", exist_ok=True)
+    for fileName in os.listdir(directoryToSolve):
+        input_filename = fileName
+        index = fileName.find('.txt')
+        output_filename = os.path.join(directoryToSolved,f'{fileName[:index]}_{algorithmType}_{order}_sol.txt')
+        additional_output_filename = os.path.join(directoryToStats, f'{fileName[:index]}_{algorithmType}_{order}_stats.txt')
+        sys.argv = [
+            'mainFifteenPuzzleGame.py', algorithmType, order, input_filename, output_filename, additional_output_filename
+        ]
+        exec(open('mainFifteenPuzzleGame.py').read())
+
+def solveDfs():
+    search_orders = ['RDUL', 'RDLU', 'DRUL', 'DRLU', 'LUDR', 'LURD', 'ULDR', 'ULRD']
+    for search_order in search_orders:
+        solveScript("dfs", search_order)
+
+def solveBfs():
+    search_orders = ['RDUL', 'RDLU', 'DRUL', 'DRLU', 'LUDR', 'LURD', 'ULDR', 'ULRD']
+    for search_order in search_orders:
+        solveScript("bfs", search_order)
+
+def aStar():
+    search_heuristics = ['manh','hamm']
+    for search_heuristic in search_heuristics:
+        solveScript("dfs", search_heuristic)
+
+if __name__ == '__main__':
+    solveDfs()
+    solveBfs()
+    aStar()
