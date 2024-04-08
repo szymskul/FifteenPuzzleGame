01ef2df0980385f74c839f7bfcd1cbb226347f63
247786 Skulimowski Szymon <247786@edu.p.lodz.pl>
2024-04-08 23:39:21 +0200

    Added statsFunctions script

--- statsFunctions.py
+++ statsFunctions.py
@@ -1,128 +1,216 @@
+import matplotlib.pyplot as plt
 import os
 
 def createPlotOfStatsOverall(numberOfLineToAdd):
-    wynikiBfs = []
-    wynikiDfs = []
-    wynikiAstar = []
-    numberOfCounts = [0,0,0]
-    countBfs = [0,0,0,0,0,0,0]
+    numberOfCountsDfs = [0,0,0,0,0,0,0]
+    numberOfCountsAstar = [0, 0, 0, 0, 0, 0, 0]
+    numberOfCountsBfs = [0,0,0,0,0,0,0]
     countDfs = [0,0,0,0,0,0,0]
-    countAstar = [0,0,0,0,0,0,0]
-    directoryToStats = "solvedStats"
+    countBfs = [0,0,0,0,0,0,0]
+    countAstar = [0, 0, 0, 0, 0, 0, 0]
+    directoryToStats = "siuuu"
     for fileName in os.listdir(directoryToStats):
-        parts = fileName.split('_')
-        with open(fileName, 'r') as file:
+        filePath = os.path.join(directoryToStats, fileName)
+        with open(filePath, 'r') as file:
             lines = file.readlines()
+            parts = fileName.split('_')
             if numberOfLineToAdd <= len(lines):
-                if(parts[2] == "bfs"):
-                    countBfs[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[0] += 1
-                elif(parts[2] == "dfs"):
-                    countDfs[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[1] += 1
-                else:
-                    countAstar[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[2] += 1
+                if(parts[3] == "sStar"):
+                    countAstar[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsAstar[int(parts[1]) - 1] += 1
+                elif(parts[3] == "Bfs"):
+                    countBfs[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsBfs[int(parts[1]) - 1] += 1
+                elif(parts[3] == "Dfs"):
+                    countDfs[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsDfs[int(parts[1]) - 1] += 1
             else:
                 return "Podana linia nie istnieje w pliku."
-    for number in countBfs:
-        wynikiBfs.append(number/numberOfCounts[0])
-    for number in countDfs:
-        wynikiDfs.append(number / numberOfCounts[1])
-    for number in countAstar:
-        wynikiAstar.append(number/numberOfCounts[2])
+    for i in range(len(countDfs)):
+        zmienna = countDfs[i]
+        dzielnik = numberOfCountsDfs[i]
+        if(dzielnik != 0):
+            countDfs[i] = zmienna / dzielnik
+        zmienna1 = countBfs[i]
+        dzielnik1 = numberOfCountsBfs[i]
+        if(dzielnik1 != 0):
+            countBfs[i] = zmienna1 / dzielnik1
+        zmienna2 = countAstar[i]
+        dzielnik2 = numberOfCountsAstar[i]
+        if(dzielnik2 != 0):
+            countAstar[i] = zmienna2 / dzielnik2
 
+    return countBfs, countDfs, countAstar
 def createPlotOfStatsAstar(numberOfLineToAdd):
-    wynikiHamm = []
-    wynikiManh = []
-    numberOfCounts = [0,0]
+    numberOfCountsHamm = [0,0,0,0,0,0,0]
+    numberOfCountsManh = [0,0,0,0,0,0,0]
     countHamm = [0,0,0,0,0,0,0]
     countManh = [0,0,0,0,0,0,0]
-    directoryToStats = "solvedStats"
+    directoryToStats = "siuuu"
     for fileName in os.listdir(directoryToStats):
-        parts = fileName.split('_')
-        with open(fileName, 'r') as file:
+        filePath = os.path.join(directoryToStats, fileName)
+        with open(filePath, 'r') as file:
             lines = file.readlines()
+            parts = fileName.split('_')
             if numberOfLineToAdd <= len(lines):
-                if(parts[2] == "hamm"):
-                    countHamm[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[0] += 1
-                elif(parts[2] == "manh"):
-                    countManh[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[1] += 1
+                if(parts[4] == "hamm"):
+                    countHamm[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    print(int(lines[numberOfLineToAdd]))
+                    numberOfCountsHamm[int(parts[1]) - 1] += 1
+                elif(parts[4] == "mnh"):
+                    countManh[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsManh[int(parts[1]) - 1] += 1
             else:
                 return "Podana linia nie istnieje w pliku."
-    for number in countHamm:
-        wynikiHamm.append(number/numberOfCounts[0])
-    for number in countManh:
-        wynikiManh.append(number / numberOfCounts[1])
+    for i in range(len(countHamm)):
+        zmienna = countHamm[i]
+        dzielnik = numberOfCountsHamm[i]
+        if(dzielnik != 0):
+            countHamm[i] = zmienna / dzielnik
+        zmienna = countManh[i]
+        dzielnik = numberOfCountsManh[i]
+        if(dzielnik != 0):
+            countManh[i] = zmienna / dzielnik
+    return countManh, countHamm
 
-def createPlotOfStatsBFSDFS(numberOfLineToAdd):
-    wynikiDRLU = []
-    wynikiRDLU = []
-    wynikiDRUL = []
-    wynikiRDUL = []
-    wynikiLUDR = []
-    wynikiULDR = []
-    wynikiLURD = []
-    wynikiULRD = []
-    numberOfCounts = [0,0,0,0,0,0,0,0]
-    countDRLU = [0,0,0,0,0,0,0]
+def createPlotOfStatsBfsDfs(numberOfLineToAdd):
+    numberOfCountsRDUL = [0,0,0,0,0,0,0]
+    numberOfCountsRDLU = [0,0,0,0,0,0,0]
+    numberOfCountsDRUL = [0,0,0,0,0,0,0]
+    numberOfCountsDRLU = [0,0,0,0,0,0,0]
+    numberOfCountsLUDR = [0,0,0,0,0,0,0]
+    numberOfCountsLURD = [0,0,0,0,0,0,0]
+    numberOfCountsULDR = [0,0,0,0,0,0,0]
+    numberOfCountsULRD = [0,0,0,0,0,0,0]
+    countRDUL = [0,0,0,0,0,0,0]
     countRDLU = [0,0,0,0,0,0,0]
     countDRUL = [0,0,0,0,0,0,0]
-    countRDUL = [0,0,0,0,0,0,0]
+    countDRLU = [0,0,0,0,0,0,0]
     countLUDR = [0,0,0,0,0,0,0]
-    countULDR = [0,0,0,0,0,0,0]
     countLURD = [0,0,0,0,0,0,0]
+    countULDR = [0,0,0,0,0,0,0]
     countULRD = [0,0,0,0,0,0,0]
-    directoryToStats = "solvedStats"
+    directoryToStats = "siuuu"
     for fileName in os.listdir(directoryToStats):
-        parts = fileName.split('_')
-        with open(fileName, 'r') as file:
+        filePath = os.path.join(directoryToStats, fileName)
+        with open(filePath, 'r') as file:
             lines = file.readlines()
+            parts = fileName.split('_')
             if numberOfLineToAdd <= len(lines):
-                if(parts[3] == "DRLU"):
-                    countDRLU[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[0] += 1
-                elif(parts[3] == "RDLU"):
-                    countRDLU[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[1] += 1
-                elif(parts[3] == "DRUL"):
-                    countDRUL[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[1] += 1
-                elif(parts[3] == "RDUL"):
-                    countRDUL[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[1] += 1
-                elif(parts[3] == "LUDR"):
-                    countLUDR[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[1] += 1
-                elif(parts[3] == "ULDR"):
-                    countULDR[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[1] += 1
-                elif(parts[3] == "LURD"):
-                    countLURD[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[1] += 1
-                else:
-                    countULRD[int(parts[1])] += lines[numberOfLineToAdd]
-                    numberOfCounts[2] += 1
+                if(parts[4] == "RDUL"):
+                    countRDUL[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsRDUL[int(parts[1]) - 1] += 1
+                elif(parts[4] == "RDLU"):
+                    countRDLU[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsRDLU[int(parts[1]) - 1] += 1
+                elif(parts[4] == "DRUL"):
+                    countDRUL[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsDRUL[int(parts[1]) - 1] += 1
+                elif (parts[4] == "DRLU"):
+                    countDRLU[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsDRLU[int(parts[1]) - 1] += 1
+                elif (parts[4] == "LUDR"):
+                    countLUDR[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsLUDR[int(parts[1]) - 1] += 1
+                elif (parts[4] == "LURD"):
+                    countLURD[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsLURD[int(parts[1]) - 1] += 1
+                elif (parts[4] == "ULDR"):
+                    countULDR[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsULDR[int(parts[1]) - 1] += 1
+                elif (parts[4] == "ULRD"):
+                    countULRD[int(parts[1]) - 1] += int(lines[numberOfLineToAdd])
+                    numberOfCountsULRD[int(parts[1]) - 1] += 1
             else:
                 return "Podana linia nie istnieje w pliku."
-    for number in countDRLU:
-        wynikiDRLU.append(number/numberOfCounts[0])
-    for number in countRDLU:
-        wynikiRDLU.append(number / numberOfCounts[1])
-    for number in countDRUL:
-        wynikiDRUL.append(number/numberOfCounts[2])
-    for number in countRDUL:
-        wynikiRDUL.append(number/numberOfCounts[0])
-    for number in countLUDR:
-        wynikiLUDR.append(number / numberOfCounts[1])
-    for number in countULDR:
-        wynikiULDR.append(number/numberOfCounts[2])
-    for number in countLURD:
-        wynikiLURD.append(number/numberOfCounts[0])
-    for number in countULRD:
-        wynikiULRD.append(number / numberOfCounts[1])
+    for i in range(len(countRDUL)):
+        zmienna = countRDUL[i]
+        dzielnik = numberOfCountsRDUL[i]
+        if(dzielnik != 0):
+            countRDUL[i] = zmienna / dzielnik
 
-#WYKRESY GENEROWANIE
+        zmienna1 = countRDLU[i]
+        dzielnik1 = numberOfCountsRDLU[i]
+        if(dzielnik1 != 0):
+            countRDLU[i] = zmienna1 / dzielnik1
 
+        zmienna2 = countDRUL[i]
+        dzielnik2 = numberOfCountsDRUL[i]
+        if(dzielnik2 != 0):
+            countDRUL[i] = zmienna2 / dzielnik2
+
+        zmienna3 = countDRLU[i]
+        dzielnik3 = numberOfCountsDRLU[i]
+        if(dzielnik3 != 0):
+            countDRLU[i] = zmienna3 / dzielnik3
+
+        zmienna4 = countLUDR[i]
+        dzielnik4 = numberOfCountsLUDR[i]
+        if(dzielnik4 != 0):
+            countLUDR[i] = zmienna4 / dzielnik4
+
+        zmienna5 = countLURD[i]
+        dzielnik5 = numberOfCountsLURD[i]
+        if(dzielnik5 != 0):
+            countLURD[i] = zmienna5 / dzielnik5
+
+        zmienna6 = countULDR[i]
+        dzielnik6 = numberOfCountsULDR[i]
+        if(dzielnik6 != 0):
+            countULDR[i] = zmienna6 / dzielnik6
+
+        zmienna7 = countULRD[i]
+        dzielnik7 = numberOfCountsULRD[i]
+        if(dzielnik2 != 0):
+            countULRD[i] = zmienna7 / dzielnik7
+    return countRDUL, countRDLU, countDRUL, countDRLU, countLUDR, countLURD, countULDR, countULRD
+
+
+def genPlotsOverall(lista1, lista2, lista3):
+    plt.figure(figsize=(10, 6))
+    bar_width = 0.25
+    indeksy = range(len(lista1))
+    plt.bar(indeksy, lista1, bar_width, label='Lista 1', color='blue')
+    plt.bar([i + bar_width for i in indeksy], lista2, bar_width, label='Lista 2', color='green')
+    plt.bar([i + 2 * bar_width for i in indeksy], lista3, bar_width, label='Lista 3', color='red')
+    plt.xlabel('Indeks')
+    plt.ylabel('Wartość')
+    plt.title('Wykres wartości dla trzech list')
+    plt.xticks([i + bar_width for i in indeksy], [i + 1 for i in indeksy])
+    plt.legend()
+    plt.grid(False)
+    plt.show()
+def genPlotsAstar(lista1, lista2):
+    plt.figure(figsize=(10, 6))
+    bar_width = 0.25
+    indeksy = range(len(lista1))
+    plt.bar(indeksy, lista1, bar_width, label='Lista 1', color='blue')
+    plt.bar([i + bar_width for i in indeksy], lista2, bar_width, label='Lista 2', color='green')
+    plt.xlabel('Indeks')
+    plt.ylabel('Wartość')
+    plt.title('Wykres wartości dla trzech list')
+    plt.xticks([i + bar_width/2 for i in indeksy], [i + 1 for i in indeksy])
+    plt.legend()
+    plt.grid(False)
+    plt.show()
+
+def genPlotsAstar(*lists):
+    plt.figure(figsize=(10, 6))
+    bar_width = 0.1
+    num_lists = len(lists)
+    indeksy = range(len(lists[0]))
+    colors = ['blue', 'green', 'red', 'orange', 'purple', 'yellow', 'cyan', 'magenta']
+
+    for i, lista in enumerate(lists):
+        plt.bar([j + i * bar_width for j in indeksy], lista, bar_width, label=f'Lista {i+1}', color=colors[i])
+
+    plt.xlabel('Indeks')
+    plt.ylabel('Wartość')
+    plt.title('Wykres wartości dla ośmiu list')
+    plt.xticks([j + (num_lists - 1) * bar_width / 2 for j in indeksy], [j + 1 for j in indeksy])
+    plt.legend()
+    plt.grid(False)
+    plt.show()
+
+countManh, countHamm = createPlotOfStatsAstar(0)
+genPlotsAstar(countManh, countHamm)
